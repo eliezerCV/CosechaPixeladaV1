d7153b72dcc627c63ce5dac2458309f8b0c8f0d2
Eliezer Cervantes <eliezer_cv21@hotmail.com>
2025-07-05 12:34:16 -0700

    feat: clase base plant y derivada Tomatoe para probar la escalabilidad

---
+++ CosechaPixeladaV1/Entities/Plants/Plant.cs
@@ -0,0 +1,19 @@
+﻿using CosechaPixeladaV1.Types;
+using Raylib_cs;
+using System.Threading.Tasks.Dataflow;
+
+namespace CosechaPixeladaV1.Entities.Plants
+{
+    public abstract class Plant
+    {
+        public int GrowthDays { get; protected set; } = 0;
+        public abstract int DaysToMature { get; }
+        protected abstract Texture2D[] Sprites { get; }
+        public bool IsMature => GrowthDays >= DaysToMature;
+
+        public virtual void AdvanceDay() => GrowthDays++;
+        public virtual int HarvestValue => 10;
+
+        
+    }
+}
---
+++ CosechaPixeladaV1/Entities/Plants/Tomato.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+
+namespace CosechaPixeladaV1.Entities.Plants
+{
+    public class Tomato : Plant
+    {
+        public override int DaysToMature => 3;
+        public override int HarvestValue => 12;
+
+        protected override Texture2D[] Sprites => throw new NotImplementedException();
+    }
+}
